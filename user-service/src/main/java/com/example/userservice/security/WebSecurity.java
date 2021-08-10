4559b89bb6538a727ee230111dbb938ea6647c37
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-08-10 19:59:03 +0900

    order-service 구현

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -12,9 +12,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
-
+        //http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                        .hasIpAddress("")
+                                .and()
+                                        .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
     }
 
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(),
+                null, null);
+        return authenticationFilter;
+    }
 }
